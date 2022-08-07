eaa13f2a840176742f55f911d18cf4e92a350916
Vladislav Selivanov <wooterland@fastmail.com>
2022-08-07 18:36:00 +0200

    [Add] Initial .oh-my-zsh config

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,8 @@
+alias du='du -sh'
+alias df='df -h'
+alias grep='grep --color=auto'
+alias ll='ls -lah'
+
+alias vim='nvim'
+
+alias dotfiles='cd ~/.dotfiles && git pull && cd -'
---
+++ .oh-my-zsh/custom/variables.zsh
@@ -0,0 +1,7 @@
+export GPG_TTY=$TTY
+export LDFLAGS="-L/opt/homebrew/opt/curl/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/curl/include"
+export PKG_CONFIG_PATH="/opt/homebrew/opt/curl/lib/pkgconfig"
+
+PATH="/opt/homebrew/opt/grep/libexec/gnubin:$PATH"
+PATH="/opt/homebrew/opt/curl/bin:$PATH"	
