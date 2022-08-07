22c84e1a168ec84048a66647a2066f030e88b07c
Vladislav Selivanov <wooterland@fastmail.com>
2022-08-07 19:25:33 +0200

    [Add] python3.10  and some other tools

--- .oh-my-zsh/custom/variables.zsh
+++ .oh-my-zsh/custom/variables.zsh
@@ -2,6 +2,8 @@ export GPG_TTY=$TTY
 export LDFLAGS="-L/opt/homebrew/opt/curl/lib"
 export CPPFLAGS="-I/opt/homebrew/opt/curl/include"
 export PKG_CONFIG_PATH="/opt/homebrew/opt/curl/lib/pkgconfig"
+export PATH="/opt/homebrew/opt/python@3.10/bin:$PATH"
+export LDFLAGS="-L/opt/homebrew/opt/python@3.10/lib"
 
 PATH="/opt/homebrew/opt/grep/libexec/gnubin:$PATH"
 PATH="/opt/homebrew/opt/curl/bin:$PATH"	
