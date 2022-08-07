21fd1cc62de0407e553c2210f4c23fc337f6c391
Vladislav Selivanov <wooterland@fastmail.com>
2022-08-07 23:36:53 +0200

    [WIP] Try make usefull alias for backups

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -4,5 +4,9 @@ alias grep='grep --color=auto'
 alias ll='ls -lah'
 alias vim='nvim'
 
-alias rcgui='rclone rcd --rc-web-gui'
 alias dotfiles='cd ~/.dotfiles && git pull && cd -'
+
+alias rcgui='rclone rcd --rc-web-gui'
+alias make_bckp='tar --use-compress-program "zstd -v" -cf $1 $2'
+alias enc_bckp='gpg -e -r "wooterland@fastmail.com" $1'
+alias send_bckp='rclone -P copy yandex:/backup/'
