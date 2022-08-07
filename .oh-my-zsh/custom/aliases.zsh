3d61b0ad07fab38132c074a68e58ee99dde88ade
Vladislav Selivanov <wooterland@fastmail.com>
2022-08-07 22:00:56 +0200

    [Add] alias for rclone web-gui

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -2,7 +2,7 @@ alias du='du -sh'
 alias df='df -h'
 alias grep='grep --color=auto'
 alias ll='ls -lah'
-
 alias vim='nvim'
 
+alias rcgui='rclone rcd --rc-web-gui'
 alias dotfiles='cd ~/.dotfiles && git pull && cd -'
